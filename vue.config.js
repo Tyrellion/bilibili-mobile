5d75fc508f2a18d01bb958312c810d2c5edc17eb
tyrellion <sasasas2002112@foxmail.com>
2024-04-19 17:15:18 +0800

    fix:格式验证关闭

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
+lintOnSave: false,
+
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  lintOnSave: false
 })
