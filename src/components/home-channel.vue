ea579d5efa354516a755f426ec1c2060030e9dea
tyrellion <sasasas2002112@foxmail.com>
2024-05-24 16:29:25 +0800

    feat:v-for渲染频道中的标签，初次使用interface来定义TS中元素类型

--- src/components/home-channel.vue
+++ src/components/home-channel.vue
@@ -1,26 +1,24 @@
-<template >
-    <van-tabs v-model:active="active">
-  <van-tab title="标签 1">内容 1</van-tab>
-  <van-tab title="标签 2">内容 2</van-tab>
-  <van-tab title="标签 3">内容 3</van-tab>
-  <van-tab title="标签 4">内容 4</van-tab>
-</van-tabs>
-
+<template>
+  <van-tabs v-model:active="active">
+    <van-tab v-for="item in list" :key="item.id" :title="item.text">内容 1</van-tab><!-- 上一个博客是直接在标签内的小标签用所有没有用上冒号 -->
+  </van-tabs>
 </template>
 <script setup lang="ts">
-import { ref } from 'vue'
-import axios from 'axios'
-const active =ref(0)
-const list =ref([])
+import { ref } from "vue";
+import axios from "axios";
+interface INavItem{
+    id:string;
+    text:string;
+}//这里定义了一个接口，用来规定list数组中的元素的类型，这样就可以在list数组中存放符合INavItem接口规定的对象了。
+const active = ref(0);
+const list = ref<INavItem[]>([]);//<INavItem[]>：这是对 list 所包含元素类型的限定，表示它应该是一个INavItem类型的数组。这里的INavItem是一个自定义的接口或类型。
 
 axios({
-    url: '/navList',
-    method:'get'
-}).then(res => {
-    console.log(res.data)
-    list.value=res.data.result
-})
+  url: "/navList",
+  method: "get",
+}).then((res) => {
+  console.log(res.data);
+  list.value = res.data.result;
+});
 </script>
-<style>
-    
-</style>
+<style></style>
