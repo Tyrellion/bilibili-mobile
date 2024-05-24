fbf037ba16073804e0b2d78b8e6593f95a53c92d
tyrellion <sasasas2002112@foxmail.com>
2024-05-24 18:04:26 +0800

    temp:视频半成

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -5,10 +5,15 @@
   <HomeChannel/>
   <!-- 轮播组件 -->
   <HomeSwipe/>
+   <!-- 视频组件 -->
+   <HomeVideoList />
+ 
 </template>
 
 <script setup lang="ts">
-import AppHeader from '@/components/app-header.vue'
+import AppHeader from '../../components/app-header.vue'
 import HomeChannel from './components/home-channel.vue'
 import HomeSwipe from './components/home-swipe.vue'
+import HomeVideoList from './components/home-video-list.vue'
+
 </script>
